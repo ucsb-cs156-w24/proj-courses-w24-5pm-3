326c214be577c1f9606485f6f2e44957eee6e2c3
Hongrui Su <hongrui_su@ucsb.edu>
2023-11-24 00:34:11 -0800

    Ray-fixbug

--- frontend/src/tests/components/Quarters/SingleQuarterDropdown.test.js
+++ frontend/src/tests/components/Quarters/SingleQuarterDropdown.test.js
@@ -164,6 +164,6 @@ describe("SingleQuarterSelector tests", () => {
       />,
     );
 
-    await waitFor(() => expect(setQuarterStateSpy).toBeCalledWith("20201"));
+    await waitFor(() => expect(setQuarterStateSpy).toBeCalledWith("sqd1", "20201"));
   });
 });
