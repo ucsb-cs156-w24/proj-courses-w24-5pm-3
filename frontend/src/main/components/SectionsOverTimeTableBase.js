be274b4ae5e29785275fe3899e4a132f488a1e42
huangderful <91812464+huangderful@users.noreply.github.com>
2023-11-26 19:29:27 -0800

    rh - reverted part of the prettier command so that prettier will pass? weird.

--- frontend/src/main/components/SectionsOverTimeTableBase.js
+++ frontend/src/main/components/SectionsOverTimeTableBase.js
@@ -54,18 +54,18 @@ export default function SectionsOverTimeTableBase({
                           background: cell.isGrouped
                             ? "#34859b"
                             : cell.isAggregated
-                              ? "#34859b"
-                              : "#9dbfbe",
+                            ? "#34859b"
+                            : "#9dbfbe",
                           color: cell.isGrouped
                             ? "#effcf4"
                             : cell.isAggregated
-                              ? "#effcf4"
-                              : "#000000",
+                            ? "#effcf4"
+                            : "#000000",
                           fontWeight: cell.isGrouped
                             ? "bold"
                             : cell.isAggregated
-                              ? "bold"
-                              : "normal",
+                            ? "bold"
+                            : "normal",
                         }}
                       >
                         {cell.isGrouped ? (
