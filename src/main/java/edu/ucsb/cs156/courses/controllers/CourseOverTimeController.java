92eda57554d6a0f679608f622f99e41bd321b84b
tigeryu8900 <tigeryu8900@gmail.com>
2023-11-27 09:27:14 -0800

    ty - REFACTOR: replace logger with @Slf4j

--- src/main/java/edu/ucsb/cs156/courses/controllers/CourseOverTimeController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/CourseOverTimeController.java
@@ -6,9 +6,6 @@ import edu.ucsb.cs156.courses.collections.ConvertedSectionCollection;
 import edu.ucsb.cs156.courses.documents.ConvertedSection;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/public/courseovertime")
 public class CourseOverTimeController {
-
-  private final Logger logger = LoggerFactory.getLogger(CourseOverTimeController.class);
 
   private ObjectMapper mapper = new ObjectMapper();
 
