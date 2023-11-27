dd919641dd5884e57742f360c44ce57658af95e8
huangderful <91812464+huangderful@users.noreply.github.com>
2023-11-26 19:04:32 -0800

    rh - finished stories and deleted more unnecessary variables

---
+++ frontend/src/stories/pages/CourseDetailsIndexPage.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import CourseDetailsIndexPage from "main/pages/CourseDetails/CourseDetailsIndexPage";
+
+export default {
+  title: "pages/CourseDetails/CourseDetailsIndexPage",
+  component: CourseDetailsIndexPage,
+  parameters: {
+    
+    qtr: "20221",
+    enrollCode: "06619"
+  }
+
+};
+
+const Template = () => <CourseDetailsIndexPage />;
+
+export const Default = Template.bind({});
