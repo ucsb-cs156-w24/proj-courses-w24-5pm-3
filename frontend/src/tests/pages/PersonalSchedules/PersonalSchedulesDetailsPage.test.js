719b318e98c364d88b3f51e1f5444229a02b86d1
huangderful <91812464+huangderful@users.noreply.github.com>
2023-11-27 21:30:01 -0800

    rh - reformatted the personal schedules details page test to use fixtures instead of being hardcoded

--- frontend/src/tests/pages/PersonalSchedules/PersonalSchedulesDetailsPage.test.js
+++ frontend/src/tests/pages/PersonalSchedules/PersonalSchedulesDetailsPage.test.js
@@ -4,6 +4,7 @@ import { MemoryRouter } from "react-router-dom";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 import PersonalSchedulesDetailsPage from "main/pages/PersonalSchedules/PersonalSchedulesDetailsPage";
+import { personalScheduleFixtures } from "fixtures/personalScheduleFixtures";
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 
@@ -74,74 +75,8 @@ describe("PersonalSchedulesDetailsPage tests", () => {
   test("shows the correct info for admin users", async () => {
     setupAdminUser();
     const queryClient = new QueryClient();
-    axiosMock.onGet(`/api/personalschedules?id=17`).reply(200, {
-      id: 17,
-      user: {
-        id: 1,
-        email: "phtcon@ucsb.edu",
-        googleSub: "115856948234298493496",
-        pictureUrl:
-          "https://lh3.googleusercontent.com/-bQynVrzVIrU/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmkGuVsELD1ZeV5iDUAUfe6_K-p8w/s96-c/photo.jpg",
-        fullName: "Phill Conrad",
-        givenName: "Phill",
-        familyName: "Conrad",
-        emailVerified: true,
-        locale: "en",
-        hostedDomain: "ucsb.edu",
-        admin: true,
-      },
-      description: "My Winter Courses",
-      quarter: "20221",
-      name: "CS156",
-    });
-    axiosMock.onGet(`api/personalSections/all?psId=17`).reply(200, [
-      {
-        quarter: "20221",
-        courseId: "ECE       1A ",
-        title: "COMP ENGR SEMINAR",
-        description:
-          "Introductory seminar to expose students to a broad range of topics in computer   engineering.",
-        classSections: [
-          {
-            enrollCode: "12583",
-            section: "0100",
-            session: null,
-            classClosed: null,
-            courseCancelled: null,
-            gradingOptionCode: null,
-            enrolledTotal: 84,
-            maxEnroll: 100,
-            secondaryStatus: null,
-            departmentApprovalRequired: false,
-            instructorApprovalRequired: false,
-            restrictionLevel: null,
-            restrictionMajor: "+PRCME+CMPEN",
-            restrictionMajorPass: null,
-            restrictionMinor: null,
-            restrictionMinorPass: null,
-            concurrentCourses: [],
-            timeLocations: [
-              {
-                room: "1930",
-                building: "BUCHN",
-                roomCapacity: "100",
-                days: "M      ",
-                beginTime: "15:00",
-                endTime: "15:50",
-              },
-            ],
-            instructors: [
-              {
-                instructor: "WANG L C",
-                functionCode: "Teaching and in charge",
-              },
-            ],
-          },
-        ],
-        generalEducation: [],
-        finalExam: null,
-      },
-    ]);
+    axiosMock.onGet(`/api/personalschedules?id=17`).reply(200, personalScheduleFixtures.onePersonalScheduleDiffId);
+    axiosMock.onGet(`api/personalSections/all?psId=17`).reply(200, personalScheduleFixtures.threePersonalSchedulesDiffId);
 
     render(
       <QueryClientProvider client={queryClient}>
