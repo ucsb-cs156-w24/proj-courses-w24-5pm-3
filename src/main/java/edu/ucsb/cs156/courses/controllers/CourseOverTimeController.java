8aca71aefabc721a184a4caa661fde58d61b700f
tigeryu8900 <tigeryu8900@gmail.com>
2023-11-27 18:03:01 -0800

    ty - reformat code

--- src/main/java/edu/ucsb/cs156/courses/controllers/CourseOverTimeController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/CourseOverTimeController.java
@@ -6,14 +6,13 @@ import edu.ucsb.cs156.courses.collections.ConvertedSectionCollection;
 import edu.ucsb.cs156.courses.documents.ConvertedSection;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/public/courseovertime")
