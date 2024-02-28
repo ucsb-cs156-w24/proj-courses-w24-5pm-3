263ba8589aee42a2d400a39deee6c550a1c7bdbc
Chloe Ta <cqta@ucsb.edu>
2024-02-28 15:14:53 -0800

    ct - Created missing stories for PSCourseCreatePage and PSCourseIndexPage.

---
+++ frontend/src/stories/pages/HomePage/Courses/PSCourseIndexPage.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import CoursesIndexPage from "main/pages/Courses/PSCourseIndexPage";
+
+export default {
+  title: "pages/Courses/PSCourseIndexPage",
+  component: CoursesIndexPage,
+};
+
+const Template = () => <CoursesIndexPage />;
+
+export const Default = Template.bind({});
