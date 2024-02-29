c426326b4b101664383c17d0055378aa6e6cd249
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-02-28 20:01:55 -0800

    removed console logs

--- frontend/src/main/components/Levels/SingleLevelDropdown.js
+++ frontend/src/main/components/Levels/SingleLevelDropdown.js
@@ -9,21 +9,16 @@ const SingleLevelDropdown = ({
   label = "Course Level",
 }) => {
   const localSearchLevel = localStorage.getItem(controlId);
-  console.log(`SingleLevelDropdown ln12: localSearchLevelFetch, controlId = ${localSearchLevel}  ${controlId}`);
   const [levelState, setLevelState] = useState(
     // Stryker disable next-line all : not sure how to test/mock local storage
     localSearchLevel || "U",
   );
-  console.log(`SingleLevelDropdown ln17: levelState = ${levelState}`);
 
   const handleLevelOnChange = (event) => {
     const selectedLevel = event.target.value;
-    console.log(`SingleLevelDropdown ln21: selectedLevel = ${selectedLevel}`);
     localStorage.setItem(controlId, selectedLevel);
     setLevelState(selectedLevel);
-    console.log(`SingleLevelDropdown ln24: controlId, setLevelState, selectedLevel  = ${controlId}  ${setLevelState}  ${selectedLevel}`);
     setLevel(selectedLevel);
-    console.log(`SingleLevelDropdown ln26: setLevel = ${setLevel}`);
     if (onChange != null) {
       onChange(event);
     }
