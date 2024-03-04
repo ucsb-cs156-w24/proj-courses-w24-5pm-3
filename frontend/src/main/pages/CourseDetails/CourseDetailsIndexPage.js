286d8fe8bbf03b2a2dda1a2bbd77d3f9d54f965c
Yerassyl Shakhmardanov <shakhmardanov@ucsb.edu>
2024-03-03 18:21:07 -0800

    added the descitption to the course to a better place :)

--- frontend/src/main/pages/CourseDetails/CourseDetailsIndexPage.js
+++ frontend/src/main/pages/CourseDetails/CourseDetailsIndexPage.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { useBackend, _useBackendMutation } from "main/utils/useBackend";
 import CourseDetailsTable from "main/components/CourseDetails/CourseDetailsTable";
 import { yyyyqToQyy } from "main/utils/quarterUtilities";
+import CourseDescription from "main/components/CourseDetails/CourseDescription";
 
 export default function CourseDetailsIndexPage() {
   // Stryker disable next-line all : Can't test state because hook is internal
@@ -33,7 +34,7 @@ export default function CourseDetailsIndexPage() {
             Course Details for {moreDetails.courseId} {yyyyqToQyy(qtr)}!
           </h5>
         )}
-
+        {moreDetails && <CourseDescription course={moreDetails} />}
         {moreDetails && <CourseDetailsTable details={[moreDetails]} />}
       </div>
     </BasicLayout>
